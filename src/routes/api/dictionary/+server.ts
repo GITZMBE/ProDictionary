0b7c7664112f7bf19360b7aafc570194e17b4c78
GITZMBE <lucasfilip@hotmail.se>
2024-06-28 15:54:35 +0200

    Add route to fetch definition of word

---
+++ src/routes/api/dictionary/+server.ts
@@ -0,0 +1,18 @@
+import type { RequestEvent } from "./$types";
+
+
+export const GET = async ({ url }: RequestEvent) => {
+  const base = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  const query = url.searchParams.get('query') || '';
+
+  const res = await fetch(base + query, {
+    method: 'GET'
+  });
+
+  if (!res.ok) {
+    return new Response('Failed to fetch definitions', { status: res.status });
+  }
+
+  const definitions = await res.json();
+  return new Response(JSON.stringify(definitions), { status: 200 });
+};
