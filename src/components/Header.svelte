fb162107e7025a33043ef347a90af84d074ac96b
GITZMBE <lucasfilip@hotmail.se>
2024-06-28 15:52:51 +0200

    Create Header and Sidebar and state to open and close sidebar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,21 @@
+<script lang="ts">
+  import icon from '$lib/assets/svelte_icon.png';
+	import Icon from '@iconify/svelte';
+  import { sidebarIsOpen } from '../routes/stores/sidebar';
 
+  $: isOpen = $sidebarIsOpen;
 
-<header class="sticky w-full p-4 bg-black text-white">
-  <h1>ProDictionary</h1>
+  const toggleSidebar = () => {
+    sidebarIsOpen.set(!isOpen);
+  }
+</script>
+
+<header class="sticky top-0 flex justify-between items-center w-full p-4 bg-secondary text-accent">
+  <a href="/" class="flex items-center gap-4">
+    <img src={icon} class="w-16" alt=""/>
+    <h1 class="text-tertiary text-3xl font-semibold">Pro<span class="text-accent">Dictionary</span></h1>
+  </a>
+  <button class="md:hidden hover:text-light overflow-hidden" on:click={toggleSidebar}>
+    <Icon icon="gg:menu" class='w-8 h-8 text-accent hover:text-light transition' />
+  </button>
 </header>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,9 +1,27 @@
+<script lang="ts">
+	import Link from "./Link.svelte";
+  import Icon from '@iconify/svelte';
+  import { sidebarIsOpen } from '../routes/stores/sidebar';
+
+  $: isOpen = $sidebarIsOpen;
+
+  const toggleSidebar = () => {
+    sidebarIsOpen.set(!isOpen);
+  }
+</script>
 
 
-<nav class="flex flex-col h-full">
-	<a href="/" class="py-2 px-4">React</a>
-  <a href="/" class="py-2 px-4">Angular</a>
-  <a href="/" class="py-2 px-4">Vue</a>
-  <a href="/" class="py-2 px-4">Svelte</a>
-  <a href="/" class="py-2 px-4">React Native</a>
-</nav>
+<aside class="group flex flex-col flex-shrink { isOpen ? 'w-[80vw] md:w-[1/3]' : 'w-0 md:w-[20vw]' } transition-all duration-300 bg-secondary text-accent overflow-y-hidden">
+  <div class="py-2 px-4 hidden md:flex justify-end">
+    <button class="{ isOpen ? '-rotate-180' : '' } transition-all duration-300 overflow-hidden" on:click={toggleSidebar}>
+      <Icon icon='ic:round-keyboard-double-arrow-right' class="w-8 h-8 text-accent" />
+    </button>
+  </div>
+  <nav class="flex flex-col">
+    <Link href="/dictionary/react">React</Link>
+    <Link href="/dictionary/angular">Angular</Link>
+    <Link href="/dictionary/vue">Vue</Link>
+    <Link href="/dictionary/svelte">Svelte</Link>
+    <Link href="/dictionary/react_native">React Native</Link>
+  </nav>
+</aside>
